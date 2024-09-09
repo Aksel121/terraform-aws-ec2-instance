8e93099d1f0e03aea0e9bdb3056ea25ba35576b0
Aksel121 <aksellutfu@gmail.com>
2024-09-09 12:30:36 -0400

    updated2

--- providers.tf
+++ providers.tf
@@ -9,8 +9,18 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
 
